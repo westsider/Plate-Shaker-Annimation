c8d1b2a7500f1f7c70c8144f23dc5abf1110b13b
Warren Hansen <whansen1@me.com>
2018-08-06 16:58:12 -0700

    Initial Commit

---
+++ Plate Shaker Annimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plate Shaker Annimation
+//
+//  Created by Warren Hansen on 8/6/18.
+//  Copyright © 2018 Warren Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
